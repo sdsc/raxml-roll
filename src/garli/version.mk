4d8fb473fce6c42d370c3f3b74a386c13035484f
Jerry Greenberg <jpg@comet-devel.sdsc.edu>
2015-06-20 21:49:28 -0700

    fixed SOURCE_NAME for garli

--- src/garli/version.mk
+++ src/garli/version.mk
@@ -6,7 +6,7 @@ TARBALL_POSTFIX    = tar.gz
 
 SRC_SUBDIR         = garli
 
-SOURCE_NAME        = $(NAME)
+SOURCE_NAME        = garli
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
