3d5cecad866ea600b8b80488f643fd69a018e0c1
Jerry Greenberg <jpg@gcn-20-88.sdsc.edu>
2015-09-07 15:34:29 -0700

    corrected path to package version.mk

--- src/raxml-8.1.24-modules/version.mk
+++ src/raxml-8.1.24-modules/version.mk
@@ -10,7 +10,7 @@ NAME        = sdsc-$(PACKAGE)-modules
 RELEASE     = 3
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
-VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)-8.1.24/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
